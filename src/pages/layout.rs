e23fd25a23757a73bb485e57ad0f5dda3475869d
MohdShahulMalik <mohdshahulmalik@gmail.com>
2026-04-19 17:19:48 +0000

    style(layout): add margin to main content area

--- src/pages/layout.rs
+++ src/pages/layout.rs
@@ -8,7 +8,7 @@ pub fn AppLayout() -> impl IntoView {
     view! {
         <div class="lg:grid lg:min-h-screen lg:grid-cols-[auto_1fr]">
             <Nav/>
-            <main class="pb-20 lg:pb-0">
+            <main class="pb-20 lg:pb-0 ml-5">
                 <Outlet/>
             </main>
         </div>
